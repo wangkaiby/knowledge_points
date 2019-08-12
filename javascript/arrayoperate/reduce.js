0c74832301d90888331064adf88d0d3f97d52b56
wangkai15 <wangkai15@100tal.com>
2019-08-12 16:35:16 +0800

    add

---
+++ javascript/arrayoperate/reduce.js
@@ -0,0 +1,26 @@
+// （1）计算数组中每个元素出现的次数
+
+let names = ['Alice', 'Bob', 'Tiff', 'Bruce', 'Alice'];
+
+let nameNum = names.reduce((pre,cur)=>{
+  if(cur in pre){
+    pre[cur]++
+  }else{
+    pre[cur] = 1 
+  }
+  return pre
+},{})
+console.log(nameNum); //{Alice: 2, Bob: 1, Tiff: 1, Bruce: 1}
+
+// （2）数组去重
+
+let arr = [1,2,3,4,4,1]
+let newArr = arr.reduce((pre,cur)=>{
+    if(!pre.includes(cur)){
+      return pre.concat(cur)
+    }else{
+      return pre
+    }
+},[])
+console.log(newArr);// [1, 2, 3, 4]
+
